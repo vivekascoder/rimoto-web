9a243079ce4a010842ef9b121b963d7504dc39bf
vivekascoder <vivekascoder@gmail.com>
2021-07-11 01:04:10 +0530

    Created the Main Web App

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {fas} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+import './assets/tailwind.css'
+import router from './router'
+
+
+library.add(fas)
+createApp(App).use(router)
+.component('fa', FontAwesomeIcon)
+.mount('#app')
